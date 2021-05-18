947bdb63cbb17f0b58a6efaec2763e6ba03a1bcb
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-05-18 15:33:43 +0530

    Initial Commit

---
+++ RakshakServer/src/TheServer/Data/User.java
@@ -0,0 +1,4 @@
+package TheServer.Data;
+
+public class User {
+}
---
+++ RakshakServer/src/TheServer/MainSection/Main.java
@@ -0,0 +1,9 @@
+package TheServer.MainSection;
+
+public class Main {
+    public static final int port = 8800;
+    private static
+    public static void main(String[] args) {
+
+    }
+}
