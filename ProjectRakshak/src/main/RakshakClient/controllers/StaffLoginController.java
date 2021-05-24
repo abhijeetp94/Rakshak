a933b70ef0a703cdbb33703e6a8ce6cb3b540a6b
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-05-24 15:20:13 +0530

    Other Login start

--- ProjectRakshak/src/main/RakshakClient/controllers/StaffLoginController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/StaffLoginController.java
@@ -1,4 +1,130 @@
 package controllers;
 
+import MainApp.Main;
+import constants.ResponseCode;
+import data.User;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import request.LoginRequest;
+import request.Response;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class StaffLoginController {
+    @FXML
+    private GridPane primaryGridPane;
+    @FXML
+    private Button loginButton, goBackButton, userLogin, adminLogin, doctorLogin;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Label headLabel;
+
+
+    public void onLoginClicked(ActionEvent ae){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if(username.trim().equals("")){
+
+        }
+        LoginRequest loginRequest = new LoginRequest(username, password);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Main.socket = new Socket(Main.ipaddress,Main.portno);
+                    Main.oosTracker = new ObjectOutputStream(Main.socket.getOutputStream());
+                    System.out.println("Object sent");
+                    Main.oosTracker.writeObject(loginRequest);
+                    Main.oisTracker = new ObjectInputStream(Main.socket.getInputStream());
+                    Response response = (Response) Main.oisTracker.readObject();
+                    if(response.getResponseCode().equals(ResponseCode.SUCCESS)){
+                        User user = (User) response.getResponseObject();
+                        System.out.println("Username = " + user.getUsername());
+                        Main.user = user;
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                headLabel.setText("Welcome " + Main.user.getFirstname() + " " + Main.user.getLastname());
+                                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                                alert.setTitle("Success");
+                                alert.setHeaderText("Login Successful.");
+                                alert.showAndWait();
+                                Stage primaryStage = (Stage) primaryGridPane.getScene().getWindow();
+                                Parent root = null;
+                                try {
+                                    root = FXMLLoader.load(getClass().getResource("/UserDashboard.fxml"));
+                                } catch (IOException ie){
+                                    ie.printStackTrace();
+                                }
+                                primaryStage.setScene(new Scene(root, 800, 600));
+                            }
+                        });
+                    }
+                    else if(response.getResponseCode().equals(ResponseCode.FAILURE)){
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                headLabel.setText("Incorrect Username or password");
+                            }
+                        });
+                    }
+                } catch (IOException ie){
+                    System.out.println("Failed while connecting to socket " + ie.getMessage());
+                } catch (ClassNotFoundException ce){
+                    System.out.println("Problem reading object: " + ce.getMessage());
+                }
+
+            }
+        }).start();
+
+    }
+
+
+    public void otherLoginClicked(ActionEvent ae){
+        if(ae.getSource().equals(userLogin)){
+            loadControl("/login.fxml");
+        } else if(ae.getSource().equals(adminLogin)){
+            loadControl("/AdminLogin.fxml");
+        }  else if(ae.getSource().equals(doctorLogin)){
+            loadControl("/DoctorLogin.fxml");
+        }else if(ae.getSource().equals(goBackButton)){
+            loadControl("/login.fxml");
+        }
+    }
+
+    public void loadControl(String filename){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Stage primaryStage = (Stage) primaryGridPane.getScene().getWindow();
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource(filename));
+
+                } catch (IOException ie){
+                    ie.printStackTrace();
+                }
+                primaryStage.setScene(new Scene(root, 800, 600));
+            }
+        });
+    }
+
 }
