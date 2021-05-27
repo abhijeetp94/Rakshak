2cb911dec7616958f4ee79064d712a35a0439adc
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-05-27 15:16:16 +0530

    button added

--- ProjectRakshak/src/main/RakshakClient/controllers/AdminDashboardController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/AdminDashboardController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import MainApp.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminDashboardController {
+    @FXML
+    private Button registerStaffButton, logoutButton, exitButton;
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private AnchorPane primaryPane;
+
+    public void initialize(){
+        welcomeLabel.setText("Welcome " + Main.user.getFirstname());
+    }
+
+    public void onRegisterClicked(ActionEvent ae){
+
+    }
+
 }
