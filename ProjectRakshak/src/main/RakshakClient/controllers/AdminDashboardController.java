e91baa83aaa750eedcf4112e9dec2701405dcded
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-05-27 15:39:29 +0530

    Logout and exit buttons added

--- ProjectRakshak/src/main/RakshakClient/controllers/AdminDashboardController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/AdminDashboardController.java
@@ -1,11 +1,14 @@
 package controllers;
 
 import MainApp.Main;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class AdminDashboardController {
     @FXML
@@ -20,6 +23,22 @@ public class AdminDashboardController {
     }
 
     public void onRegisterClicked(ActionEvent ae){
+        Main.loadControl(primaryPane, "/AdminRegister.fxml");
+    }
+
+    public void onToolBarButtonClicked(ActionEvent ae){
+        if(ae.getSource().equals(logoutButton)){
+            Main.user = null;
+            try {
+                Main.socket.close();
+            } catch (IOException ie){
+                System.out.println("Error Logging out: " + ie.getMessage());
+            }
+            Main.loadControl(primaryPane, "/login.fxml");
+        }
+        if(ae.getSource().equals(exitButton)){
+            Platform.exit();
+        }
 
     }
 
