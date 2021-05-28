edebceaa50897b7b23723607172f06a2f229ddcc
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-05-28 15:35:15 +0530

    IDs added

--- ProjectRakshak/src/main/RakshakClient/controllers/AdminRegisterController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/AdminRegisterController.java
@@ -2,11 +2,25 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 
 public class AdminRegisterController {
     @FXML
     private Tab staffTab, adminTab, doctorTab;
     @FXML
-    private Button logoutButton, exitButton, goBackButton;
+    private Button logoutButton, exitButton, goBackButton, registerStaffButton, registerDoctorButton, registerAdminButton;
+    @FXML
+    private TextField uidField, staffIDField, firstNameField, lastNameField, emailField, titleField, baseSalaryField, gradePayField;
+    @FXML
+    private PasswordField passwordField, rePasswordField, passwordFieldDoctor, rePasswordFieldDoctor, passwordFieldAdmin, rePasswordFieldAdmin;
+    @FXML
+    private TextField uidFieldDoctor, staffIDFieldDoctor, firstNameFieldDoctor, lastNameFieldDoctor, emailFieldDoctor, titleFieldDoctor, baseSalaryFieldDoctor, gradePayFieldDoctor, experienceFieldDoctor, educationFieldDoctor;
+    @FXML
+    private TextField uidFieldAdmin, staffIDFieldAdmin, firstNameFieldAdmin, lastNameFieldAdmin, emailFieldAdmin, titleFieldAdmin, baseSalaryFieldAdmin, gradePayFieldAdmin;
+
+
+
+
 }
