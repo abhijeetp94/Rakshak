4bd362d8cc190bffea7d12730a8b3acba2c995ea
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-06-01 18:14:14 +0530

    Changes in dashboard

--- ProjectRakshak/src/main/RakshakClient/controllers/DoctorDashboardController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/DoctorDashboardController.java
@@ -1,4 +1,46 @@
 package controllers;
 
+import MainApp.Main;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class DoctorDashboardController {
+    @FXML
+    private AnchorPane primaryPane;
+    @FXML
+    private Button exitButton, logoutButton, goBackButton, accountButton, scheduleButton, payManagerButton, contactButton, attendanceButton;
+    @FXML
+    private Label dateLabel;
+    @FXML
+    private ListView staffScheduleList;
+
+    public void onToolBarButtonClicked(ActionEvent ae){
+        if(ae.getSource().equals(logoutButton)){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Logout");
+            alert.setHeaderText("Do you want to logout?");
+            alert.showAndWait().ifPresent(response -> {
+                if(response.equals(ButtonType.OK)){
+                    Main.user = null;
+                    try {
+                        Main.socket.close();
+                    } catch (IOException ie){
+                        System.out.println("Error Logging out: " + ie.getMessage());
+                    }
+                    Main.loadControl(primaryPane, "/login.fxml");
+                }
+            });
+
+        }
+        if(ae.getSource().equals(exitButton)){
+            Main.user = null;
+            Platform.exit();
+        }
+
+    }
 }
--- ProjectRakshak/src/main/classesAPI/data/StaffSchedule.java
+++ ProjectRakshak/src/main/classesAPI/data/StaffSchedule.java
@@ -10,4 +10,42 @@ public class StaffSchedule implements Serializable {
     LocalDate date;
     LocalTime time;
 
+    public StaffSchedule(String title, String description, LocalDate date, LocalTime time) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
 }
