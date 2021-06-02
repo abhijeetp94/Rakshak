539064ae14a0150b78402f15d74549eaffa92d11
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-06-02 19:17:02 +0530

    Have to study more javafx man

--- ProjectRakshak/src/main/RakshakClient/controllers/TimeTableDialogController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/TimeTableDialogController.java
@@ -18,10 +18,17 @@ public class TimeTableDialogController {
     TableView<TimeTable> doctorTableView;
     @FXML
     private TableColumn<TimeTable, String> doctorColumn, fieldColumn;
+    @FXML
     private TableColumn<TimeTable, LocalTime> startTimeColumn, endTimeColumn;
+    @FXML
     private TableColumn<TimeTable, Boolean> availableColumn;
 
     public void setData(ObservableList<TimeTable> timeTables){
-        doctorColumn.setCellValueFactory(new PropertyValueFactory<TimeTable, String>("firstName"));
+        doctorColumn.setCellValueFactory(new PropertyValueFactory<TimeTable, String>("doctor.fullName"));
+        fieldColumn.setCellValueFactory(new PropertyValueFactory<TimeTable, String>("doctor.title"));
+        startTimeColumn.setCellValueFactory(new PropertyValueFactory<TimeTable, LocalTime>("shiftStartTime"));
+        endTimeColumn.setCellValueFactory(new PropertyValueFactory<TimeTable, LocalTime>("shiftEndTime"));
+        availableColumn.setCellValueFactory(new PropertyValueFactory<TimeTable, Boolean>("doctor.isAvailable"));
+        doctorTableView.setItems(timeTables);
     }
 }
