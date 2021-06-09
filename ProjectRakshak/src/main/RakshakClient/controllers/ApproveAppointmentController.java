735fff24623ec2124e754b31bfd6cbf80068c1d4
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-06-09 16:10:26 +0530

    Approve appointments extended

--- ProjectRakshak/src/main/RakshakClient/controllers/ApproveAppointmentController.java
+++ ProjectRakshak/src/main/RakshakClient/controllers/ApproveAppointmentController.java
@@ -1,4 +1,43 @@
 package controllers;
 
+import data.Doctor;
+import data.Schedule;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Pair;
+
+import java.awt.*;
+
 public class ApproveAppointmentController {
+    @FXML
+    private AnchorPane primaryPane;
+    @FXML
+    private Button approveAllButton, approveSelectedButton;
+    @FXML
+    private TableView<Schedule> scheduleTableView;
+    @FXML
+    private TableView<Doctor> doctorTableView;
+    @FXML
+    private TableColumn<Schedule, String> doctorColumn, userColumn;
+    @FXML
+    private TableColumn<Schedule, Integer> slotColumn;
+    @FXML
+    private TableColumn<Doctor, Integer> shiftColumn, patientColumn;
+    @FXML
+    private TableColumn<Doctor, String> doctorDetailsColumn;
+    @FXML
+    private ContextMenu contextMenu;
+
+    public void initialize(){
+        scheduleTableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        scheduleTableView.getColumns().setAll(doctorColumn, slotColumn, userColumn);
+
+        doctorTableView.getColumns().setAll(doctorDetailsColumn, shiftColumn, patientColumn);
+    }
+
 }
